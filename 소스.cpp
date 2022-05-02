b901fd2f44aedba659fdae67fc198a1fe09ee746
sinsung <sinsung0505@naver.com>
2022-05-02 10:20:45 +0900

    first

--- 소스.cpp
+++ 소스.cpp
@@ -7,26 +7,26 @@ int main()
 {
 	int Money = 0;
 	Money = 100;
+	int Operand = 10;
 
-	
-	cout << Money + 3 << endl;
-	cout << Money - 3 << endl;
-	cout << Money * 3 << endl;
-	cout << Money / 3 << endl;
-	cout << Money % 3 << endl;
+	cout << Money + Operand << endl;
+	cout << Money - Operand << endl;
+	cout << Money * Operand << endl;
+	cout << Money / Operand << endl;
+	cout << Money % Operand << endl;
 
 
 	float Mygold = 0;
 	Mygold = 0.1f;
 
 	cout << Money + 0.3f << endl;
-	
+
 	cout << Money * 0.3f << endl;
 	cout << Money / 0.3f << endl;
 
 
 	return 0;
 
-	
+
 
 }
