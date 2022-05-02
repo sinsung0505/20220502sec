5c8ab45aeb76e791e468c2ea8e4c160d2e1f443f
sinsung <sinsung0505@naver.com>
2022-05-02 09:35:32 +0900

    first

---
+++ 소스.cpp
@@ -0,0 +1,22 @@
+# include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+	int Money = 0;
+	Money = 100;
+
+	
+		cout << Money + 3 << endl;
+	cout << Money - 3 << endl;
+	cout << Money * 3 << endl;
+	cout << Money / 3 << endl;
+	cout << Money % 3 << endl;
+
+	return 0;
+
+	
+
+}
